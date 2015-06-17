56fd5b4c0281e35dc4aa1a0197e0409b5c2f49b4
isra17 <isra017@gmail.com>
2015-06-16 23:45:07 -0400

    Set api_client game to null on delete

--- hah/models/api_client.py
+++ hah/models/api_client.py
@@ -11,7 +11,7 @@ class ApiClient(db.Model):
 
     id =	    db.Column(db.Integer, primary_key=True)
 
-    game_id =       db.Column(db.Integer, db.ForeignKey('games.id'), nullable=True)
+    game_id =       db.Column(db.Integer, db.ForeignKey('games.id', ondelete="SET NULL"), nullable=True)
     game =          db.relationship("Game", uselist=False)
 
     shared_secret = db.Column(db.String(length=88))
